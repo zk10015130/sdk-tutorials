02dee2013714b97e4c2a61aea984e31492be03a2
zk10015130 <62981384+zk10015130@users.noreply.github.com>
2020-05-14 10:40:35 +0900

    Update main.go

--- nameservice/cmd/nsd/main.go
+++ nameservice/cmd/nsd/main.go
@@ -24,7 +24,7 @@ import (
 
 	gaiaInit "github.com/cosmos/cosmos-sdk/cmd/gaia/init"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	app "github.com/zk10015130/nameservice"
+	app "github.com/zk10015130/sdk-tutorials/nameservice"
 	abci "github.com/tendermint/tendermint/abci/types"
 	cfg "github.com/tendermint/tendermint/config"
 	dbm "github.com/tendermint/tendermint/libs/db"
